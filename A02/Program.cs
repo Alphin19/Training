82b7b9616e818c318571d506d71df03f990dfdb8
alphin <Alphinkamernisha.Nagoormeeran@trumpf.com>
2026-07-30 17:35:56 +0530

    Review changes made 1

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,12 +1,17 @@
-﻿//Program for Guessing a Number Between 1 and 100
-int secretNumber =  new Random.Next (1, 101);
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2026.
+// Copyright (c) Metamation India.
+// -------------------------------------------------------------------------------------------------
+// Program.cs
+// Program to generate a random number between 1 and 100 and allow the user to guess it.
+int secretNumber =  new Random().Next (1, 101);
 int guess;
 int attempts = 0;
 Console.WriteLine ("I have generated a number between 1 and 100.");
 do {
    Console.Write ("Enter your guess: ");
-   if (!int.TryParse (input, out guess)) 
-   {
+   string? input = Console.ReadLine ();
+   if (!int.TryParse (input, out guess)) {
       Console.WriteLine ("Please enter a valid number.");
       continue;
    }
