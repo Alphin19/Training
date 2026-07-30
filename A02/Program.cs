d86319fe7c358113645cf2e4a912652d626ec9be
alphin <Alphinkamernisha.Nagoormeeran@trumpf.com>
2026-07-30 15:56:03 +0530

    review comments fixed

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,26 +1,21 @@
-﻿using System;
-
-Random random = new Random ();
-int secretNumber = random.Next (1, 101);
+﻿//Program for Guessing a Number Between 1 and 100
+int secretNumber =  new Random.Next (1, 101);
 int guess;
 int attempts = 0;
-
 Console.WriteLine ("I have generated a number between 1 and 100.");
-
 do {
    Console.Write ("Enter your guess: ");
-   guess = Convert.ToInt32 (Console.ReadLine ());
-
+   if (!int.TryParse (input, out guess)) 
+   {
+      Console.WriteLine ("Please enter a valid number.");
+      continue;
+   }
    attempts++;
-
-   if (guess > secretNumber) {
-      Console.WriteLine ("Too High!");
-   } else if (guess < secretNumber) {
-      Console.WriteLine ("Too Low!");
-   } else {
+   if (guess > secretNumber) Console.WriteLine ("Your guess is too high!");
+   else if (guess < secretNumber) Console.WriteLine ("You guess is too Low!");
+   else {
       Console.WriteLine ("Correct!");
       Console.WriteLine ($"You guessed it in {attempts} attempts.");
    }
-
 } while (guess != secretNumber); 
 
