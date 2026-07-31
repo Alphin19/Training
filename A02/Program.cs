4df1d5bacedb38ca1784c938f2f3f00edf06e759
alphin <Alphinkamernisha.Nagoormeeran@trumpf.com>
2026-07-31 14:16:58 +0530

    Revert "Formatting review comments fixed"
    This reverts commit bb94d4a07f7f1afa0e3ea6e81935fb4eabd5ad4e.

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,10 +1,11 @@
 ﻿// ------------------------------------------------------------------------------------------------
 // Training ~ A training program for new joinees at Metamation, Batch- July 2026.
 // Copyright (c) Metamation India.
-// -------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // Program.cs
 // Program to generate a random number between 1 and 100 and allow the user to guess it.
-int secretNumber =  new Random().Next (1, 101);
+// ------------------------------------------------------------------------------------------------
+int secretNumber = new Random ().Next (1, 101);
 int guess;
 int attempts = 0;
 Console.WriteLine ("I have generated a number between 1 and 100.");
@@ -17,7 +18,7 @@ do {
    }
    attempts++;
    if (guess > secretNumber) Console.WriteLine ("Your guess is too high!");
-   else if (guess < secretNumber) Console.WriteLine ("You guess is too Low!");
+   else if (guess < secretNumber) Console.WriteLine ("Your guess is too low!");
    else {
       Console.WriteLine ("Correct!");
       Console.WriteLine ($"You guessed it in {attempts} attempts.");
