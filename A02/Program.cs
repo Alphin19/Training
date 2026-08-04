8e1a1703bb23dee33ccb4afb5c7d39906fac2c69
alphin <Alphinkamernisha.Nagoormeeran@trumpf.com>
2026-08-04 15:08:42 +0530

    Addressed review comment

--- A02/Program.cs
+++ A02/Program.cs
@@ -6,21 +6,24 @@
 // Program to generate a random number between 1 and 100 and allow the user to guess it.
 // ------------------------------------------------------------------------------------------------
 int secretNumber = new Random ().Next (1, 101);
-int guess;
 int attempts = 0;
 Console.WriteLine ("I have generated a number between 1 and 100.");
-do {
+while (true) {
    Console.Write ("Enter your guess: ");
    string? input = Console.ReadLine ();
-   if (!int.TryParse (input, out guess)) {
+   if (!int.TryParse (input, out int guess)) {
       Console.WriteLine ("Please enter a valid number.");
+      continue;
+   }
+   if (guess < 1 || guess > 100) {
+      Console.WriteLine ("Please enter a number between 1 and 100.");
       continue;
    }
    attempts++;
    if (guess > secretNumber) Console.WriteLine ("Your guess is too high!");
    else if (guess < secretNumber) Console.WriteLine ("Your guess is too low!");
    else {
-      Console.WriteLine ("Correct!");
-      Console.WriteLine ($"You guessed it in {attempts} attempts.");
+      Console.WriteLine ($"Correct!,you guessed it in {attempts} attempts");
+      break;
    }
-} while (guess != secretNumber);
+}
