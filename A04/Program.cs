cb3bbba07c6de54630a886497541a8de66289dee
alphin <Alphinkamernisha.Nagoormeeran@trumpf.com>
2026-08-20 14:39:07 +0530

    Spelling bee - Letter Frequency

---
+++ A04/Program.cs
@@ -0,0 +1,15 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------------------------------------
+// Program.cs
+// Spelling Bee - Letter Frequency
+// ------------------------------------------------------------------------------------------------
+string[] words = File.ReadAllLines ("words.txt");
+Dictionary<char, int> freq = new ();
+foreach (string word in words) {
+   foreach (char c in word.ToUpper ())
+      if (c >= 'A' && c <= 'Z') freq[c] = freq.GetValueOrDefault (c) + 1;
+}
+foreach (var ch in freq.OrderByDescending (a => a.Value).Take (7))
+   Console.WriteLine ($"{ch.Key} : {ch.Value}");
